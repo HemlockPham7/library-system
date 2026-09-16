888bc64477f5d5c84efc76814b1a960d0f0be84b
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-09-16 07:01:53 +0700

    set up discovery server

---
+++ discoveryserver/src/test/java/com/librarysystem/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarysystem.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
